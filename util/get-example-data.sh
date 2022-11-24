4b5352743445922596083e291a5531319ce56427
Julian Kosciessa <julian.kosciessa@donders.ru.nl>
2022-11-24 17:44:34 +0100

    include bash script to get data

---
+++ util/get-example-data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# download continuous .wav recording of robot counting (https://freesound.org/people/EnjoyPA/sounds/203066/; CC0)
+wget https://zenodo.org/record/7358140/files/Counting-from-1-to-20.wav?download=1 -O Counting-from-1-to-20.wav
+# download segmented stroop mat file
+wget https://zenodo.org/record/7358140/files/stroop_example.mat?download=1 -O stroop_example.mat
